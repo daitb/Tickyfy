44b6aa91521a4063b4524892f3ff2d54ae11f34b
Tran Ba Dai <tranbadai1234@gmail.com>
2025-11-19 10:01:40 +0700

    fix: error call user api

--- Source/Tickify.Server/Interfaces/Services/IAdminService.cs
+++ Source/Tickify.Server/Interfaces/Services/IAdminService.cs
@@ -4,9 +4,4 @@ namespace Tickify.Interfaces.Services;
 
 public interface IAdminService
 {
-    Task<List<UserListDto>> GetAllUsersAsync(int pageNumber, int pageSize);
-    Task UpdateUserRoleAsync(int userId, string roleName);
-    Task<List<OrganizerRequestDto>> GetOrganizerRequestsAsync();
-    Task ApproveOrganizerRequestAsync(int requestId, int adminId);
-    Task RejectOrganizerRequestAsync(int requestId, int adminId);
 }
